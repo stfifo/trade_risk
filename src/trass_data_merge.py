7f211b35a3b075ee23d3c70f069db265187a60e1
choijieun <eaee20171@gmail.com>
2026-04-05 22:23:46 +0900

    Refactor: update gitignore and add data check notebook

--- src/trass_data_merge.py
+++ src/trass_data_merge.py
@@ -10,7 +10,7 @@ if not file_list:
 else:
     print(f"total {len(file_list)} file found")
 
-# 2. 빈 리스트를 만들고, 파일들을 읽어서 리스트에 차곡차곡 담기
+
 df_list = []
 
 for file in file_list:
@@ -23,7 +23,7 @@ for file in file_list:
 
 merged_df = pd.concat(df_list, ignore_index=True)
 merged_df = merged_df[merged_df['기간'] != '총계']
-print(f"\n병합 완료! 총 데이터 행 개수: {len(merged_df)}줄")
+print(f"\n병합 완료... 총 데이터 행 개수: {len(merged_df)}")
 
 save_path = "../data/merged_trade_data.csv"
 merged_df.to_csv(save_path, index=False, encoding='utf-8-sig')
